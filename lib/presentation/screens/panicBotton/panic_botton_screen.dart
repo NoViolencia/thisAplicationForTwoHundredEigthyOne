758b92eaacc5667d347cd50e782bc7cdcf082c72
Roberth-HQ <rocahuqui@gmail.com>
2023-11-15 16:32:47 -0400

    boton del panico ya implementado

--- lib/presentation/screens/panicBotton/panic_botton_screen.dart
+++ lib/presentation/screens/panicBotton/panic_botton_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:nueva_app/data/models/usuario_normal_model.dart';
+import 'package:nueva_app/presentation/screens/widget/shared/app_Bar_generic.dart';
+import 'package:nueva_app/presentation/screens/widget/shared/navigation_botton.dart';
 import 'package:nueva_app/services/contactos_services.dart';
 import 'package:nueva_app/services/institucionAyuda_services.dart';
 import 'package:nueva_app/services/usuario_normal_services.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_sms/flutter_sms.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PanicBottonScreen extends StatelessWidget {
   const PanicBottonScreen({Key? key});
@@ -12,11 +16,10 @@ class PanicBottonScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int idUsuario=71;
+    //int idUsuario=71;
+    final int idUsuario = ModalRoute.of(context)!.settings.arguments as int;
     return Scaffold(
-      appBar: AppBar(
-        title:const Text('Boton del pánico oo'),
-      ),
+      appBar: AppBarGeneric(nombreAppBar: 'Boton Del Panico',idUser: idUsuario),
       backgroundColor: Colors.white70,
       body: Center(
         child: ElevatedButton(
@@ -41,6 +44,8 @@ class PanicBottonScreen extends StatelessWidget {
           ),
         ),
       ),
+
+      bottomNavigationBar: NavigationBotton(indice:2,idUser: idUsuario),
     );
   }
 
@@ -104,7 +109,7 @@ void _enviarMensaje(Data user, List<int> contactos, List<int> instituciones) {
   List<String> telefonosInstituciones = instituciones.map((telefono) => telefono.toString()).toList();
 
   // Cuerpo del mensaje
-  String mensaje = 'Ayuda urgente en la dirección: ${user.usuario.direccion}';
+  String mensaje = 'Hola soy ${user.usuario.nombre} Necesito Ayuda urgente!! vivo en la dirección: ${user.usuario.direccion} pero actualmente estoy en: ';
 
   // Combinar todos los números de teléfono
   List<String> todosLosTelefonos = [...telefonosContactos, ...telefonosInstituciones];
@@ -113,27 +118,95 @@ void _enviarMensaje(Data user, List<int> contactos, List<int> instituciones) {
   _enviarSMS(todosLosTelefonos, mensaje);
 }
 
+
+
+
 void _enviarSMS(List<String> destinatarios, String mensaje) async {
   try {
-    String result = await sendSMS(
-      message: mensaje,
-      recipients: destinatarios,
-    );
-    print(result);
+    // Verificar y solicitar permisos de ubicación
+    var status = await Permission.location.status;
+    if (status != PermissionStatus.granted) {
+      await Permission.location.request();
+      status = await Permission.location.status;
+    }
+
+    if (status == PermissionStatus.granted) {
+      // Obtener la ubicación del usuario
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      // Construir el enlace de Google Maps con las coordenadas de la ubicación
+      String ubicacionLink =
+          "https://www.google.com/maps/search/?api=1&query=${position.latitude},${position.longitude}";
+
+      // Enviar SMS con el enlace de ubicación
+      String result = await sendSMS(
+        message: "$mensaje\n$ubicacionLink",
+        recipients: destinatarios,
+      );
+
+      print(result);
+    } else {
+      print('El usuario no otorgó los permisos necesarios para obtener la ubicación.');
+    }
   } catch (error) {
     print('Error al enviar SMS: $error');
   }
-
 }
 
 
+// void _enviarSMS(List<String> destinatarios, String mensaje) async {
+//   try {
+//     // Verificar y solicitar permisos de ubicación
+//     var status = await Permission.location.status;
+//     if (status != PermissionStatus.granted) {
+//       await Permission.location.request();
+//       status = await Permission.location.status;
+//     }
+
+//     if (status == PermissionStatus.granted) {
+//       // Obtener la ubicación del usuario
+//       Position position = await Geolocator.getCurrentPosition(
+//         desiredAccuracy: LocationAccuracy.high,
+//       );
+
+//       String ubicacion = "Ubicación: ${position.latitude}, ${position.longitude}";
+
+//       // Combinar la ubicación con el mensaje
+//       String mensajeConUbicacion = "$mensaje\n$ubicacion";
+
+//       // Enviar SMS
+//       String result = await sendSMS(
+//         message: mensajeConUbicacion,
+//         recipients: destinatarios,
+//       );
+
+//       print(result);
+//     } else {
+//       print('El usuario no otorgó los permisos necesarios para obtener la ubicación.');
+//     }
+//   } catch (error) {
+//     print('Error al enviar SMS: $error');
+//   }
+// }
 
 
 
 
 
+// void _enviarSMS(List<String> destinatarios, String mensaje) async {
+//   try {
+//     String result = await sendSMS(
+//       message: mensaje,
+//       recipients: destinatarios,
+//     );
+//     print(result);
+//   } catch (error) {
+//     print('Error al enviar SMS: $error');
+//   }
 
-
+// }
 
 
 
